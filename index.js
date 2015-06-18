98fb2bd0fe21f2b80034da3d3ca01cc710198a55
Cassiohg <cassiohg@gmail.com>
2015-06-18 06:18:06 -0300

    added call to spark
    server is now calling spark, subimmiting jar, reading text file and sending its content back as response

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var exec = require('child_process').exec
+var fs = require ('fs')
 var express = require('express');
 var app = express();
 
@@ -17,11 +19,27 @@ app.use('/', function (req, res, next) {
 });
 
 // accept POST request on the homepage
-app.get('/', function (req, res, next) {
+app.get('/speedLimit', function (req, res, next) {
 	// var newPerson = new Person(newPersonID)
+	var command = '/usr/local/share/spark-1.3.1-bin-hadoop2.6/bin/spark-submit ' +
+				  '/Users/cassiohg/Coding/Scala/riobus-report/target/scala-2.10/riobus-report_2.10-1.0.jar'
+	var spark = exec(command, function (error, stdout, stderr) {
+		// console.log('stdout: ' + stdout);
+		console.log('stderr: ' + stderr);
+		if (error !== null) {
+			console.log('exec error: ' + error);
+		}
 
-	res.json({'metrica1': {'descricao': 'quantidade de onibus a cima da velocidade permitida', 
-						  'valor': 292}
+		fs.readFile('/Users/cassiohg/Downloads/speedLimit-result.txt', 'utf8', function (err, data) {
+			if (err) throw err;
+			res.json({'metrica1': {'description': 'quantidade de onibus a cima da velocidade permitida', 
+						 			    'sample': data
+						 		  }
+		});
+	});
+
+	// res.json({'metrica1': {'description': 'quantidade de onibus a cima da velocidade permitida', 
+	// 					  'sample': 292}
 	
 	})
 });
